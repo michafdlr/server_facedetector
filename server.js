344b9ed955a300ca264027b94aa4b4a50e5f0aa3
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-06-02 13:54:34 +0200

    added reset password endpoint

--- server.js
+++ server.js
@@ -25,29 +25,6 @@ const storeHashInDatabase = (hash) => {
   // Store the hash in your password DB
   return hash // For now we are returning the hash for testing at the bottom
 }
-const hash1 = await storeUserPassword("12345", saltRounds)
-const hash2 = await storeUserPassword("09876", saltRounds)
-
-const database = {
-  users: [
-    {
-      "id": "1",
-      "name": "michael",
-      "email": "michael@gmail.com",
-      "password": hash1,
-      "counter": 0,
-      "joined": new Date()
-    },
-    {
-      "id": "2",
-      "name": "laura",
-      "email": "laura@gmail.com",
-      "password": hash2,
-      "counter": 0,
-      "joined": new Date()
-    }
-  ]
-}
 
 const checkUserPassword = async (enteredPassword, storedPasswordHash) =>
   await bcrypt.compare(enteredPassword, storedPasswordHash)
@@ -84,25 +61,6 @@ const addUser = async (name, email, password, res) => {
       .catch(trx.rollback)
   })
   .catch(err => res.status(400).json("unable to register"))
-
-
-  // await db('login')
-  //   .insert({
-  //     hash: hash,
-  //     email: email
-  //   })
-
-  // db('users')
-  //   .returning('*')
-  //   .insert({
-  //     name: name,
-  //     email: email,
-  //     joined: new Date()
-  //   })
-  //   .then(user => {
-  //     res.json(user[0])
-  //   })
-  //   .catch(err => res.status(400).json("unable to register"))
 }
 
 // const getUser = async (id) => {
@@ -160,6 +118,22 @@ app.put("/image", (req, res) => {
     .returning('counter')
     .then(counter => res.json(counter[0].counter))
     .catch(err => res.status(400).json('could not get user'))
+})
+
+app.put("/reset", async (req, res) => {
+  const {email, password} = req.body
+  db('login').select().where('email', '=', email).returning('*')
+    .then(async response => {
+      if (!response.length) {
+        res.status(400).json('invalid email')
+      } else {
+        const hash = await storeUserPassword(password, saltRounds)
+        db('login').where('email', '=', email).update({
+          hash: hash
+        }, ['email'])
+          .then(loginEmail => res.status(200).json(loginEmail[0].email))
+      }
+    })
 })
 
 app.listen(PORT, () => `App running on port ${PORT}`)
